7455376777aebbd7cfcc63bed9aa47a04e5d9076
Kallam Taylor <kallam_taylor@hotmail.com>
2025-09-05 04:18:15 +0100

    HomePage Layout Updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,22 @@
 import './Home.css'
+import SignUpForm from '../../components/SignUpForm/SignupForm'
+
+import BoxingGloveImg from '/src/assets/boxing-gloves.jpg'
 
 export default function Home() {
   return (
+    <>
     <div className="home">
       <h1>Welcome to the Boxing Training Tracker</h1>
+  
       <p>Track your training progress and stay motivated!</p>
     </div>
+
+    <section className="form-section column">
+      <img className="form-feature" src={BoxingGloveImg} alt="Boxing Glove" />
+       <SignUpForm />
+    </section>
+
+    </>
   )
 }
